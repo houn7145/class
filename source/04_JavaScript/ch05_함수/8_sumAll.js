0a016587ad9b32baee58bbbdb525d9f74ab30f25
houn7145 <houn7145@naver.com>
2023-01-25 22:24:26 +0900

    source

---
+++ source/04_JavaScript/ch05_함수/8_sumAll.js
@@ -0,0 +1,11 @@
+function sumAll () {
+    var result = 0;
+    if(arguments.length == 0){
+        result = -999;
+    }else {
+        for(var idx in arguments){
+            result+=arguments[idx];         
+        }
+    }
+    return result;
+}
